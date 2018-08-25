4a19e2f0e2cbab0922cd41f99d0ddd78253bc3a4
FDI <man.xiegc@gmail.com>
2018-08-25 23:56:14 +0800

    1

--- css/gclab.js
+++ css/gclab.js
@@ -5,12 +5,12 @@ var snect = "<div id=\"tooplate_header_fp\">"+
 "    	<div id=\"tooplate_menu\" class=\"ddsmoothmenu\">"+
 "            <ul>"+
 "               <li><a href=\"../\" class=\"selected\"><span></span>Home</a></li>"+
-"				<li><a href=\"../about.html\"><span></span>Research</a></li>"+
+"				<li><a href=\"../issue/research.html\"><span></span>Research</a></li>"+
 "				<li><a href=\"../pubs/index.html\"><span></span>Publications</a></li>"+
-"				<li><a href=\"../doc/index.html\"><span></span>Resources</a></li>"+
+"				<li><a href=\"../res/index.html\"><span></span>Resources</a></li>"+
 "				<li><a href=\"../blog\"><span></span>Blog</a></li>"+
-"               <li><a href=\"portfolio.html\"><span></span>Portfolio</a></li>"+
-"               <li><a href=\"../contact.html\"><span></span>Contact</a></li>"+
+"               <li><a href=\"../activity/portfolio.html\"><span></span>Portfolio</a></li>"+
+"               <li><a href=\"../issue/contact.html\"><span></span>Contact</a></li>"+
 "            </ul>"+
 "            <br style=\"clear: left\" />"+
 "        </div>"+
@@ -34,12 +34,12 @@ var snect = "<div id=\"tooplate_header_sp\">"+
 "    	<div id=\"tooplate_menu\" class=\"ddsmoothmenu\">"+
 "            <ul>"+
 "               <li><a href=\"../\"><span></span>Home</a></li>"+
-"				<li><a href=\"../about.html\" class=\"selected\"><span></span>Research</a></li>"+
+"				<li><a href=\"../issue/research.html\" class=\"selected\"><span></span>Research</a></li>"+
 "				<li><a href=\"../pubs/index.html\"><span></span>Publications</a></li>"+
-"				<li><a href=\"../doc/index.html\"><span></span>Resources</a></li>"+
+"				<li><a href=\"../res/index.html\"><span></span>Resources</a></li>"+
 "				<li><a href=\"../blog\"><span></span>Blog</a></li>"+
-"               <li><a href=\"portfolio.html\"><span></span>Portfolio</a></li>"+
-"               <li><a href=\"../contact.html\"><span></span>Contact</a></li>"+
+"               <li><a href=\"../activity/portfolio.html\"><span></span>Portfolio</a></li>"+
+"               <li><a href=\"../issue/contact.html\"><span></span>Contact</a></li>"+
 "            </ul>"+
 "            <br style=\"clear: left\" />"+
 "        </div>"+
@@ -49,8 +49,89 @@ var snect = "<div id=\"tooplate_header_sp\">"+
 document.writeln(snect);
 }
 
+function subtop_publication()
+{
+var snect = "<div id=\"tooplate_header_sp\">"+
+"    	<div id=\"tooplate_menu\" class=\"ddsmoothmenu\">"+
+"            <ul>"+
+"               <li><a href=\"../\"><span></span>Home</a></li>"+
+"				<li><a href=\"../issue/research.html\"><span></span>Research</a></li>"+
+"				<li><a href=\"../pubs/index.html\" class=\"selected\"><span></span>Publications</a></li>"+
+"				<li><a href=\"../res/index.html\"><span></span>Resources</a></li>"+
+"				<li><a href=\"../blog\"><span></span>Blog</a></li>"+
+"               <li><a href=\"../activity/portfolio.html\"><span></span>Portfolio</a></li>"+
+"               <li><a href=\"../issue/contact.html\"><span></span>Contact</a></li>"+
+"            </ul>"+
+"            <br style=\"clear: left\" />"+
+"        </div>"+
+"       <div id=\"site_title\"><h1><a href=\"../\">Guancai Xie Homepage</a></h1></div>"+
+"		</div>"+
+"    </div>";
+document.writeln(snect);
+}
 
+function subtop_resource()
+{
+var snect = "<div id=\"tooplate_header_sp\">"+
+"    	<div id=\"tooplate_menu\" class=\"ddsmoothmenu\">"+
+"            <ul>"+
+"               <li><a href=\"../\"><span></span>Home</a></li>"+
+"				<li><a href=\"../issue/research.html\"><span></span>Research</a></li>"+
+"				<li><a href=\"../pubs/index.html\"><span></span>Publications</a></li>"+
+"				<li><a href=\"../res/index.html\" class=\"selected\"><span></span>Resources</a></li>"+
+"				<li><a href=\"../blog\"><span></span>Blog</a></li>"+
+"               <li><a href=\"../activity/portfolio.html\"><span></span>Portfolio</a></li>"+
+"               <li><a href=\"../issue/contact.html\"><span></span>Contact</a></li>"+
+"            </ul>"+
+"            <br style=\"clear: left\" />"+
+"        </div>"+
+"       <div id=\"site_title\"><h1><a href=\"../\">Guancai Xie Homepage</a></h1></div>"+
+"		</div>"+
+"    </div>";
+document.writeln(snect);
+}
 
+function subtop_contact()
+{
+var snect = "<div id=\"tooplate_header_sp\">"+
+"    	<div id=\"tooplate_menu\" class=\"ddsmoothmenu\">"+
+"            <ul>"+
+"               <li><a href=\"../\"><span></span>Home</a></li>"+
+"				<li><a href=\"../about.html\"><span></span>Research</a></li>"+
+"				<li><a href=\"../pubs/index.html\"><span></span>Publications</a></li>"+
+"				<li><a href=\"../doc/index.html\"><span></span>Resources</a></li>"+
+"				<li><a href=\"../blog\"><span></span>Blog</a></li>"+
+"               <li><a href=\"portfolio.html\"><span></span>Portfolio</a></li>"+
+"               <li><a href=\"../contact.html\" class=\"selected\"><span></span>Contact</a></li>"+
+"            </ul>"+
+"            <br style=\"clear: left\" />"+
+"        </div>"+
+"       <div id=\"site_title\"><h1><a href=\"../\">Guancai Xie Homepage</a></h1></div>"+
+"		</div>"+
+"    </div>";
+document.writeln(snect);
+}
+
+function subtop_activity()
+{
+var snect = "<div id=\"tooplate_header_sp\">"+
+"    	<div id=\"tooplate_menu\" class=\"ddsmoothmenu\">"+
+"            <ul>"+
+"               <li><a href=\"../\"><span></span>Home</a></li>"+
+"				<li><a href=\"../about.html\"><span></span>Research</a></li>"+
+"				<li><a href=\"../pubs/index.html\"><span></span>Publications</a></li>"+
+"				<li><a href=\"../doc/index.html\"><span></span>Resources</a></li>"+
+"				<li><a href=\"../blog\"><span></span>Blog</a></li>"+
+"               <li><a href=\"portfolio.html\" class=\"selected\"><span></span>Portfolio</a></li>"+
+"               <li><a href=\"../contact.html\"><span></span>Contact</a></li>"+
+"            </ul>"+
+"            <br style=\"clear: left\" />"+
+"        </div>"+
+"       <div id=\"site_title\"><h1><a href=\"../\">Guancai Xie Homepage</a></h1></div>"+
+"		</div>"+
+"    </div>";
+document.writeln(snect);
+}
 //------简单底部------------------------------------
 function main_footer()
 {
