308b4c299987fc3bef8e6f0a7be43e935b07032b
FDI <man.xiegc@gmail.com>
2018-08-25 23:26:40 +0800

    1

---
+++ css/gclab.js
@@ -0,0 +1,176 @@
+//------首页menu-----------------------------------------------
+function home_top()
+{
+var snect = "<div id=\"tooplate_header_fp\">"+
+"    	<div id=\"tooplate_menu\" class=\"ddsmoothmenu\">"+
+"            <ul>"+
+"               <li><a href=\"../\" class=\"selected\"><span></span>Home</a></li>"+
+"				<li><a href=\"../about.html\"><span></span>Research</a></li>"+
+"				<li><a href=\"../pubs/index.html\"><span></span>Publications</a></li>"+
+"				<li><a href=\"../doc/index.html\"><span></span>Resources</a></li>"+
+"				<li><a href=\"../blog\"><span></span>Blog</a></li>"+
+"               <li><a href=\"portfolio.html\"><span></span>Portfolio</a></li>"+
+"               <li><a href=\"../contact.html\"><span></span>Contact</a></li>"+
+"            </ul>"+
+"            <br style=\"clear: left\" />"+
+"        </div>"+
+"       <div id=\"site_title\"><h1><a href=\"../\">Guancai Xie Homepage</a></h1></div>"+
+"         <div id=\"tooplate_slider\">"+
+"            <div id=\"flash_grid_slider\">"+
+"                <a href=\"http://www.adobe.com/go/getflashplayer\">"+
+"                    <img src=\"http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif\" alt=\"Get Adobe Flash player\" />"+
+"                </a>"+
+"            </div>"+
+"		  </div>"+
+"		</div>"+
+"    </div>";
+document.writeln(snect);
+}
+
+//------分页menu---------------------------------------------------
+function childpage_research()
+{
+var snect = "<div id=\"tooplate_header_sp\">"+
+"    	<div id=\"tooplate_menu\" class=\"ddsmoothmenu\">"+
+"            <ul>"+
+"               <li><a href=\"../\"><span></span>Home</a></li>"+
+"				<li><a href=\"../about.html\" class=\"selected\"><span></span>Research</a></li>"+
+"				<li><a href=\"../pubs/index.html\"><span></span>Publications</a></li>"+
+"				<li><a href=\"../doc/index.html\"><span></span>Resources</a></li>"+
+"				<li><a href=\"../blog\"><span></span>Blog</a></li>"+
+"               <li><a href=\"portfolio.html\"><span></span>Portfolio</a></li>"+
+"               <li><a href=\"../contact.html\"><span></span>Contact</a></li>"+
+"            </ul>"+
+"            <br style=\"clear: left\" />"+
+"        </div>"+
+"       <div id=\"site_title\"><h1><a href=\"../\">Guancai Xie Homepage</a></h1></div>"+
+"		</div>"+
+"    </div>";
+document.writeln(snect);
+}
+
+function childpage_contact()
+{
+var snect = "<div id=\"banner\">"+
+"  <EMBED pluginspage=http://www.macromedia.com/go/getflashplayer src=../CSS/banner_bg.swf   width=\"970px\" height=\"100px\" type=application/x-shockwave-flash wmode=\"transparent\" quality=\"high\">"+
+"</div>"+
+"<div id=\"menu\" class=\"menu\">"+
+"  <ul class=\"menuL\">"+
+"    <li><a href=\"../\" target=\"_self\">Homepage</a></li>"+
+"    <li><a href=\"../issue/group.html\" target=\"_self\">Group</a></li>"+
+"    <li><a class=\"now\" href=\"../blog\" target=\"_self\">Research</a></li>"+
+"    <li><a href=\"../issue/tools.html\" target=\"_self\">Tools</a></li>"+
+"    <li><a href=\"../pubs/index.html\" target=\"_self\">Publications</a></li>"+
+"    <li><a href=\"../activity\" target=\"_self\">Activity</a></li>"+
+"    <li class=\"last\"><a href=\"../issue/links.html\" target=\"_blank\">Links</a></li>"+
+"  </ul>"+
+"  <ul class=\"menuR\">"+
+"    <li><a href=# onClick=\"window.external.addFavorite('../','Guancai's Homepage')\">->>shoucang</a></li>"+
+"    <li class=\"last\"><a href=# onClick=\"this.style.behavior='url(#default#homepage)';this.setHomePage('../');\">->>shoucang</a></li>"+
+"  </ul>"+
+"</div>";
+document.writeln(snect);
+}
+function childpage_tool()
+{
+var snect = "<div id=\"banner\">"+
+"  <EMBED pluginspage=http://www.macromedia.com/go/getflashplayer src=../CSS/banner_bg.swf   width=\"970px\" height=\"100px\" type=application/x-shockwave-flash wmode=\"transparent\" quality=\"high\">"+
+"</div>"+
+"<div id=\"menu\" class=\"menu\">"+
+"  <ul class=\"menuL\">"+
+"    <li><a href=\"../\" target=\"_self\">Homepage</a></li>"+
+"    <li><a href=\"../issue/group.html\" target=\"_self\">Group</a></li>"+
+"    <li><a href=\"../blog\" target=\"_self\">Research</a></li>"+
+"    <li><a class=\"now\" href=\"../issue/tools.html\" target=\"_self\">Tools</a></li>"+
+"    <li><a href=\"../pubs/index.html\" target=\"_self\">Publications</a></li>"+
+"    <li><a href=\"../activity\" target=\"_self\">Activity</a></li>"+
+"    <li class=\"last\"><a href=\"../issue/links.html\" target=\"_blank\">Links</a></li>"+
+"  </ul>"+
+"  <ul class=\"menuR\">"+
+"    <li><a href=# onClick=\"window.external.addFavorite('../','Guancai's Homepage')\">->>shoucang</a></li>"+
+"    <li class=\"last\"><a href=# onClick=\"this.style.behavior='url(#default#homepage)';this.setHomePage('../');\">->>shoucang</a></li>"+
+"  </ul>"+
+"</div>";
+document.writeln(snect);
+}
+
+function childpage_publication()
+{
+var snect = "<div id=\"banner\">"+
+"  <EMBED pluginspage=http://www.macromedia.com/go/getflashplayer src=../CSS/banner_bg.swf   width=\"970px\" height=\"100px\" type=application/x-shockwave-flash wmode=\"transparent\" quality=\"high\">"+
+"</div>"+
+"<div id=\"menu\" class=\"menu\">"+
+"  <ul class=\"menuL\">"+
+"    <li><a href=\"../\" target=\"_self\">Homepage</a></li>"+
+"    <li><a href=\"../issue/group.html\" target=\"_self\">Group</a></li>"+
+"    <li><a href=\"../blog\" target=\"_self\">Research</a></li>"+
+"    <li><a href=\"../issue/tools.html\" target=\"_self\">Tools</a></li>"+
+"    <li><a class=\"now\" href=\"../pubs/index.html\" target=\"_self\">Publications</a></li>"+
+"    <li><a href=\"../activity\" target=\"_self\">Activity</a></li>"+
+"    <li class=\"last\"><a href=\"../issue/links.html\" target=\"_blank\">Links</a></li>"+
+"  </ul>"+
+"  <ul class=\"menuR\">"+
+"    <li><a href=# onClick=\"window.external.addFavorite('../','Guancai's Homepage')\">->>shoucang</a></li>"+
+"    <li class=\"last\"><a href=# onClick=\"this.style.behavior='url(#default#homepage)';this.setHomePage('../');\">->>shoucang</a></li>"+
+"  </ul>"+
+"</div>";
+document.writeln(snect);
+}
+
+function childpage_activity()
+{
+var snect = "<div id=\"banner\">"+
+"<EMBED pluginspage=http://www.macromedia.com/go/getflashplayer src=../CSS/banner_bg.swf   width=\"970px\" height=\"100px\" type=application/x-shockwave-flash wmode=\"transparent\" quality=\"high\">"+
+"</div>"+
+"<div id=\"menu\" class=\"menu\">"+
+"  <ul class=\"menuL\">"+
+"    <li><a href=\"../\" target=\"_self\">Homepage</a></li>"+
+"    <li><a href=\"../issue/group.html\" target=\"_self\">Group</a></li>"+
+"    <li><a href=\"../blog\" target=\"_self\">Research</a></li>"+
+"    <li><a href=\"../issue/tools.html\" target=\"_self\">Tools</a></li>"+
+"    <li><a href=\"../pubs/index.html\" target=\"_self\">Publications</a></li>"+
+"    <li><a class=\"now\" href=\"../activity\" target=\"_self\">Activity</a></li>"+
+"    <li class=\"last\"><a href=\"../issue/links.html\" target=\"_blank\">Links</a></li>"+
+"  </ul>"+
+"  <ul class=\"menuR\">"+
+"    <li><a href=# onClick=\"window.external.addFavorite('../','Guancai's Homepage')\">->>shoucang</a></li>"+
+"    <li class=\"last\"><a href=# onClick=\"this.style.behavior='url(#default#homepage)';this.setHomePage('../');\">->>shoucang</a></li>"+
+"  </ul>"+
+"</div>";
+document.writeln(snect);
+}
+
+function childpage_top0()
+{
+var snect = "<div id=\"banner\">"+
+"<EMBED pluginspage=http://www.macromedia.com/go/getflashplayer src=../CSS/banner_bg.swf   width=\"970px\" height=\"100px\" type=application/x-shockwave-flash wmode=\"transparent\" quality=\"high\">"+
+"</div>"+
+"<div id=\"menu\" class=\"menu\">"+
+"  <ul class=\"menuL\">"+
+"    <li><a class=\"now\" href=\"../\" target=\"_self\">Homepage</a></li>"+
+"    <li><a href=\"../issue/group.html\" target=\"_self\">Group</a></li>"+
+"    <li><a href=\"../blog\" target=\"_self\">Research</a></li>"+
+"    <li><a href=\"../issue/tools.html\" target=\"_self\">Tools</a></li>"+
+"    <li><a href=\"../pubs/index.html\" target=\"_self\">Publications</a></li>"+
+"    <li><a href=\"../activity\" target=\"_self\">Activity</a></li>"+
+"    <li class=\"last\"><a href=\"../issue/links.html\" target=\"_blank\">Links</a></li>"+
+"  </ul>"+
+"  <ul class=\"menuR\">"+
+"    <li><a href=# onClick=\"window.external.addFavorite('../','Guancai's Homepage')\">->>shoucang</a></li>"+
+"    <li class=\"last\"><a href=# onClick=\"this.style.behavior='url(#default#homepage)';this.setHomePage('../');\">->>shoucang</a></li>"+
+"  </ul>"+
+"</div>";
+document.writeln(snect);
+}
+
+//------简单底部------------------------------------
+function main_footer()
+{
+var snect = "<div id=\"footer\">"+
+"	<div id=\"tooplate_footer_wrapper\">"+
+"		<div id=\"tooplate_footer\">"+
+"			Copyright © 2018-2028 <a href=./ target=_self>Guancai Xie Homepage</a>"+
+"		</div>"+
+"	</div>";
+document.writeln(snect);
+}
